08abec1aa26cf8f4e27126aa21f9e72f0157a7af
Kinam Kim <63842546+kinam0252@users.noreply.github.com>
2024-12-20 00:24:15 +0900

    Update pipeline_stg_mochi.py

--- diffusers/mochi/pipeline_stg_mochi.py
+++ diffusers/mochi/pipeline_stg_mochi.py
@@ -207,7 +207,7 @@ class STGMochiAttnProcessor2_0:
             full_mask = torch.zeros((full_sequence_length_ptb, full_sequence_length_ptb), device=query_ptb.device, dtype=query_ptb.dtype)
             
             full_mask[:identity_block_size, :identity_block_size] = float("-inf")
-            full_mask[:identity_block_size, identity_block_size:].fill_diagonal_(0)
+            full_mask[:identity_block_size, :identity_block_size].fill_diagonal_(0)
             
             full_mask = full_mask.unsqueeze(0).unsqueeze(0)
 
